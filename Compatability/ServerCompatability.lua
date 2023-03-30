6f357ee6469802738b8a93795a7b2d98463591dd
Austin <austin.m.babcock@gmail.com>
2023-03-30 09:14:08 -0500

    added error checking

--- Compatability/ServerCompatability.lua
+++ Compatability/ServerCompatability.lua
@@ -1,10 +1,18 @@
 --#region QB
 Inventory.QB.AddItem = function (src,item,amount)
-		local player = QBCore.Functions.GetPlayer(src)
-		return player.Functions.AddItem(item, amount or 1)
+	local player = QBCore.Functions.GetPlayer(src)
+	if not player.Functions.AddItem(item, amount or 1) then
+		PrintUtils.PrintError("Item Does not Exist for Player: "..Color.Yellow..item)
+		return false
 	end
+	return true
+end
 
 Inventory.QB.RemoveItem = function (src, item, amount)
+	if not QBCore.Shared.Items[item] then
+		PrintUtils.PrintError("Item Does not Exist"..Color.Yellow..item)
+		return nil
+	end
 	local player = QBCore.Functions.GetPlayer(src)
 	return player.Functions.RemoveItem(item, amount or 1)
 end
