81971f6950cb8e72c72959f01f0ef72194778e98
Austinb96 <austin.m.babcock@gmaIl.com>
2023-04-02 18:23:49 +0000

    Added ItemSetup functions

--- Compatability/ServerCompatability.lua
+++ Compatability/ServerCompatability.lua
@@ -26,4 +26,30 @@ Inventory.OX.AddItem = function (src,item,amount)
 Inventory.OX.RemoveItem = function (src, item, amount)
 	return ox_inventory:RemoveItem(src,item,amount)
 end
+
+
+ItemSetup = {
+	AddItems = function(items)
+		local AddItems = {}
+		for key, data in pairs(items) do
+			if not QBCore.Shared.Items[key] then
+				AddItems[key] = data
+			else
+				PrintUtils.PrintWarning("Item allready exists: "..Color.White..key)
+			end
+		end
+
+		local bool, message, errorItem = QBCore.Functions.AddItems(AddItems)
+		if not bool then
+			PrintUtils.PrintError("Item Import Failed!: ("..message.."). Please import manualy")
+			for key, value in pairs(errorItem) do
+				if key == 'name' then
+					PrintUtils.PrintError("Item that failed: "..Color.White..value)
+				end
+			end
+		else
+			PrintUtils.PrintDebug("Item Import Successful")
+		end
+	end
+}
 --#endregion
