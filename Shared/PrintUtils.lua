ca350a14b733dbedf155bbc28532a8a8a4b0cfed
Austinb96 <austin.m.babcock@gmaIl.com>
2023-04-17 16:29:10 +0000

    Added util for printing tables

--- Shared/PrintUtils.lua
+++ Shared/PrintUtils.lua
@@ -117,7 +117,15 @@ function PrintUtils.PrintMultiDebug(table)
     PrintUtils.PrintMulti(table, Color.Violet, "Debug: ")
 end
 
-function PrintUtils.PrintErrorDebug(text)
-    if not canPrint() then return end
-	PrintUtils.PrintError(text)
+function PrintUtils.PrintTable(table, indent)
+    indent = indent or 0
+    for k, v in pairs(table) do
+        local formatting = string.rep("  ", indent) .. k .. ": "
+        if type(v) == "table" then
+            print(formatting)
+            PrintUtils.PrintTable(v, indent+1)
+        else
+            print(formatting .. tostring(v))
+end
+    end
 end
