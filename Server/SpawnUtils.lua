a78c5cd32d78952513e252a94a0e0f5f59bd7a07
Austinb96 <austin.m.babcock@gmaIl.com>
2023-04-20 13:14:28 +0000

    removed client sided functions

--- Server/SpawnUtils.lua
+++ Server/SpawnUtils.lua
@@ -1,19 +1,16 @@
 function SpawnVeh(model, pos, rot, src, teleportInto)
-	model = type(model) == 'string' and GetHashKey(model) or model
-	Utils.LoadModel(model)
     local vehicle = CreateVehicle(model, pos.x, pos.y, pos.z, rot or 0, true, true)
 	local netId = nil
 	while not DoesEntityExist(vehicle) or not netId do
 		Wait(0)
+		if DoesEntityExist(vehicle) then
 		netId = NetworkGetNetworkIdFromEntity(vehicle)
 	end
-
-	SetNetworkIdCanMigrate(netid, true)
-	SetVehicleFuelLevel(veh, 100.0)
-
-	SetModelAsNoLongerNeeded(model)
+	end
 
     local plate = GetVehicleNumberPlateText(vehicle)
+	if teleportInto then TaskWarpPedIntoVehicle(src, vehicle, -1) end
+
     PrintUtils.PrintDebug({
 		{"Vehicle spawn Info: NetId:"},
 		{netId, Color.White},
@@ -23,6 +20,5 @@ function SpawnVeh(model, pos, rot, src, teleportInto)
 		{pos, Color.White}
 	})
 
-	if teleportInto then TaskWarpPedIntoVehicle(src, vehicle, -1) end
     return netId, plate
 end
