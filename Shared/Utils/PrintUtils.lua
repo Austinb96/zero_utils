1fe73512a7fc8952b0533ec3a4c437aab143b433
Austinb96 <austin.m.babcock@gmaIl.com>
2023-05-09 05:07:17 +0000

    Added documentation to printutils

--- Shared/Utils/PrintUtils.lua
+++ Shared/Utils/PrintUtils.lua
@@ -23,24 +23,26 @@ function PrintUtils.GetColors()
 	return Color
 end
 
---Example usage of PrintUtils.Print
+--- @alias ColorTypes '^0' | '^1' | '^2' | '^3' | '^4' | '^5' | '^6'
 
---// Print with everything one color //--
--- PrintUtils.Print("Testing Print", PrintColors.Red)
-
---// Used to print MultiColoredLine //--
--- PrintUtils.Print(
---     {
---         {text = "Testing", color = PrintColors.Green },
---         {text = "Print", color = PrintColors.Violet }
---     }
--- )
-
---// Or you can print MultiColor like this //--
---PrintUtils.Print(PrintColors.Red .. "this will also work ".. PrintColors.DarkBlue .. "Just like this")
+--- - `text` (`string|table`)
+--- - (Optional)`color` (`string | Color`): color for entire print line
+--- - (Optional)`prefix` (`string`): Prefix for print line
+--- -
+---@param text string|table
+---@param color? ColorTypes
+---@param prefix? string
+---@usage
+--- Example 1: PrintUtils.Print("Hello", Color.Red)
+--- -
+---@usage
+--- Example 2: PrintUtils.Print({"Hello", Color.Red}, nil, "ExamplePrefix:")
+--- -
+---@usage
+--- Example 3: PrintUtils.Print({{"Hello"},{"World", Color.Yellow}}, Color.Red)
 function PrintUtils.Print(text, color, prefix)
 	color = color or Color.White
-	prefix = prefix or ''
+	prefix = prefix.." " or ''
     local output = ''
 	local defaultColor = color
     if type(text) == "table" then
@@ -55,17 +57,20 @@ function PrintUtils.Print(text, color, prefix)
     print(defaultColor .. prefix .. output..Color.White)
 end
 
-
---Used to print multiple lines of Print. eg.
-
--- PrintUtils.PrintMulti(
---     {
---         {{text = "Testing", color = PrintColors.Green},{text = "Multi", color = PrintColors.Yellow}},  //use this to print multiColored Lines
---         {text = "Print", color = PrintColors.Violet }      //next table item will print to new line
---     }
--- )
-
+--- Will print text to multiple lines
+--- - (Optional)`color` (`string | Color`): color for entire print lines
+--- - (Optional)`prefix` (`string`): Prefix for print lines
+--- -
+---@param table table
+---@param color? ColorTypes
+---@param prefix? string
+---@usage
+--- Example 1: PrintUtils.PrintMulti({"Hello", Color.Red}, nil, "ExamplePrefix:")
+--- -
+---@usage
+--- Example 2: PrintUtils.PrintMulti({{"Hello"},{"World", Color.Yellow}}, Color.Red)
 function PrintUtils.PrintMulti(table, color, prefix)
+	if type(table) ~= "table" then PrintUtils.PrintError("Expected table got: "..Color.white..type(table)..":"..tostring(table))end
 	local text
     for _, item in pairs(table) do
 		text = item.text or item[1] or item
@@ -81,7 +86,12 @@ function PrintUtils.PrintMulti(table, color, prefix)
     end
 end
 
-
+--- Will print out red text with "Error:" prefix along with a trace
+--- - Does not stop execution
+--- -
+---@param text string
+---@usage
+--- Example 1: PrintUtils.PrintError("This is an Error")
 function PrintUtils.PrintError(text)
     local trace = Citizen.InvokeNative(`FORMAT_STACK_TRACE` & 0xFFFFFFFF, nil, 1, Citizen.ResultAsString())
 	local traceLines = {}
@@ -92,33 +102,64 @@ function PrintUtils.PrintError(text)
 	PrintUtils.PrintMulti(
 		{
 			{text, Color.Red, "Error: "},
-			{traceLines, Color.Red, "Error: "},
+			{traceLines, Color.Red, "Error:"},
 		}
 	)
 end
 
+
+--- Print Warning in Yellow text and "Warning!:" prefix
+--- - (Optional)`showIf` (`boolean`): boolean to pass in if you want the option to hide
+--- -
+---@param text string
+---@param showIf boolean
+---@usage
+--- Example 1: PrintUtils.PrintWarning("This is an Warning!")
 function PrintUtils.PrintWarning(text, showIf)
     if showIf and showIf == false then return end
-    PrintUtils.Print(text, Color.Yellow,"Warning!!: ")
+    PrintUtils.Print(text, Color.Yellow,"Warning!:")
 end
 
+--- Print Warning in Yellow text and "Warning!:" prefix for multiple lines
+--- - (Optional)`showIf` (`boolean`): boolean to pass in if you want the option to hide
+--- -
+---@param table string
+---@param showIf boolean
+---@see PrintUtils.PrintMulti
 function PrintUtils.PrintMultiWarning(table, showIf)
     if showIf and showIf == false then return end
-    PrintUtils.PrintMulti(table, Color.Yellow, "Warning: ")
+    PrintUtils.PrintMulti(table, Color.Yellow, "Warning!:")
 end
 
+
+--- Print Debug in Green text and "Debug:" prefix
+--- - (Optional)`showDebug` (`boolean`): option to overwrite bool from when printutils was setup
+--- -
+---@param text string
+---@param showIf boolean
+---@usage
+--- Example 1: PrintUtils.PrintDebug("This is an Debug Message")
 function PrintUtils.PrintDebug(text, showDebug)
 	showDebug = showDebug or CanPrintDebug()
     if not showDebug then return end
-    PrintUtils.Print(text, Color.Green, Color.Violet.."Debug: ")
+    PrintUtils.Print(text, Color.Green, Color.Violet.."Debug:")
 end
 
+
+--- Print Debug in Green text and "Debug:" prefix
+--- - (Optional)`showDebug` (`boolean`): option to overwrite bool from when printutils was setup
+--- -
+---@param table table
+---@param showDebug boolean
+---@see PrintUtils.PrintMulti
 function PrintUtils.PrintMultiDebug(table, showDebug)
     showDebug = showDebug or CanPrintDebug()
     if not showDebug then return end
-    PrintUtils.PrintMulti(table, Color.Green, "Debug: ")
+    PrintUtils.PrintMulti(table, Color.Green, "Debug:")
 end
 
+---Prints out a table for debuging
+---@param table table
 function PrintUtils.PrintTable(table, indent)
     indent = indent or 0
     for k, v in pairs(table) do
