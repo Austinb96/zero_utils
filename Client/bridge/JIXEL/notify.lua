ee67b0d58edf9c014656ec99d4d7699bf1d412ec
Austinb96 <austin.m.babcock@gmaIl.com>
2024-07-16 20:00:22 -0500

    Delete Client directory

--- Client/bridge/JIXEL/notify.lua
+++
@@ -1,7 +0,0 @@
-if not AssertBridgeResource(Config.Bridge.Notify, "jixel", "jixel-notify") then return end
-
-local notify = function (type,message,options)
-    exports['jixel-notify']:J(type, message, options?.length or 5000, options)
-end
-
-return notify
