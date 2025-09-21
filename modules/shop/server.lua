b6d92a92bd4797c3bec824a7ae81d90e0ff7efb5
Austinb96 <austins.m.babcock@gmail.com>
2025-09-20 20:23:09 -0500

    chore: shop changes from tay

--- modules/shop/server.lua
+++ modules/shop/server.lua
@@ -4,8 +4,7 @@ if not shop then return end
 zutils.shop = {}
 
 function zutils.shop.registerShop(id, shopData)
-    printdb(true, "Registering shop with ID: %s", id)
-    printtable(shopData)
+    printdb("Registering shop with ID: %s", id)
     shop.registerShop(id, shopData)
 end
 
