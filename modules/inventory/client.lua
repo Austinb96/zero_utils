109ccdf2ee1b02d178782ac8e708e699a3d73857
taylorrenee1 <itstaylorrenee1@gmail.com>
2026-02-21 20:23:28 -0800

    chore(inventory): add findItem

--- modules/inventory/client.lua
+++ modules/inventory/client.lua
@@ -24,6 +24,10 @@ end
 
 zutils.inventory.HasItem = zutils.inventory.hasItem -- Alias for compatibility
 
+function zutils.inventory.findItem(item, metadata)
+    return inventory.findItem(item, metadata)
+end
+
 function zutils.inventory.getItemInfo(item)
     return inventory.getItemInfo(item)
 end
